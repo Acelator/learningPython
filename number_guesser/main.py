f624753ab5d4e27c0115802b332e02f95fbaefd7
Acelator <Corbla04@gmail.com>
2021-03-01 00:51:46 +0100

    Number guesser: V1.1 | Capture ValueError

--- number_guesser/main.py
+++ number_guesser/main.py
@@ -5,11 +5,10 @@ import sys
 def main():
     print("Guess a number between 0 and 1000")
     number = random.randint(0, 1000)
-
     split = [int(i) for i in str(number)]
 
     print("Enter first guess")
-    guess1 = int(input())
+    guess1: int = guess()
 
     if guess1 == number:
         print("You have win")
@@ -19,7 +18,7 @@ def main():
         print("The number starts with " + str(split[0]))
 
         print("Enter second guess")
-        guess2 = int(input())
+        guess2: int = guess()
 
         if guess2 == number:
             print("You have win")
@@ -29,7 +28,7 @@ def main():
             print("Here is another hint " + str(split[0]) + str(split[1]))
 
             print("Enter third guess")
-            guess3 = int(input())
+            guess3: int = guess()
 
             if guess3 == number:
                 print("You have win")
@@ -39,4 +38,22 @@ def main():
                 print("The number was " + str(number))
 
 
+# Collects the user type
+def guess():
+    # Checks that a number is introduced
+    try:
+        value = int(input())
+        return value
+    except ValueError:
+        print("You didn't introduce a number")
+        print("Let's try again. Introduce a number")
+        try:
+            value = int(input())
+            return value
+        except ValueError:
+            print("You have again introduce a non valid answer. Start again the game in case you want to play")
+            sys.exit()
+
+
+# Starts the app
 main()
