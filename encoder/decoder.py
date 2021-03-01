e1ef633e676efff815b45a3b1bd517fab98e67db
Acelator <Corbla04@gmail.com>
2021-03-01 21:05:36 +0100

    Encoder: V1.0.1 | Add some comments

--- encoder/decoder.py
+++ encoder/decoder.py
@@ -1,11 +1,13 @@
 import dic
 
 
+# Decode a set value
 def decode(value: str, method: int or str):
     decoded_value = map(decoder, value)
     joined = "".join(map(str, list(decoded_value)))
     print(joined)
 
 
+# It returns the original value
 def decoder(item):
     return dic.inverseDefaultValue.get(item)
